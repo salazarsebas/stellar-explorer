9734776f79e42fda31e84058cdb1a7f99b391858
Miguel Nieto <39246879+miguelnietoa@users.noreply.github.com>
2026-03-21 14:57:48 -0500

    chore: remove root migrations/ moved to indexer/migrations/

--- migrations/012_operations_muxed_accounts.down.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE operations
-    DROP COLUMN IF EXISTS source_account_muxed,
-    DROP COLUMN IF EXISTS source_muxed_id,
-    DROP COLUMN IF EXISTS destination_muxed,
-    DROP COLUMN IF EXISTS destination_muxed_id;
--- migrations/013_transactions_muxed_id.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE transactions
-    DROP COLUMN IF EXISTS account_muxed_id;
